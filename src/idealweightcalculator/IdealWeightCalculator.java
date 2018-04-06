2a1ffb31d60262572a594511f7aec6fc1f2de59c
clkong <36968245+clkong@users.noreply.github.com>
2018-04-06 12:41:17 -0400

    Update IdealWeightCalculator.java

--- src/idealweightcalculator/IdealWeightCalculator.java
+++ src/idealweightcalculator/IdealWeightCalculator.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Sean Luo 06/04/2018
+ * IdealWeightCalculator.java
+ * This progrem calculates the IdealWeightCalculator
  */
 
 package idealweightcalculator;
